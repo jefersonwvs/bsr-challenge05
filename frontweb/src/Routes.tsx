ed47012e7197034f0f45393fc3b1172c1b6cc3a4
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2021-11-05 10:41:53 -0400

    Refactoring routes

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -2,9 +2,9 @@ import { Router, Switch, Route, Redirect } from 'react-router-dom';
 import PrivateRoute from 'components/PrivateRoute';
 import history from 'utils/history';
 import Navbar from 'components/Navbar';
-import Auth from 'pages/Home/Auth';
-import Catalog from 'pages/Home/Catalog';
-import MovieDetails from 'pages/Home/MovieDetails';
+import Auth from 'pages/Auth';
+import Catalog from 'pages/Catalog';
+import MovieDetails from 'pages/MovieDetails';
 
 const Routes = function () {
    return (
@@ -12,18 +12,15 @@ const Routes = function () {
          <Navbar />
          <Switch>
             <Redirect from="/" to="/movies" exact />
-            {/*<Route path="/" exact>
-               <Home />
-   </Route>*/}
+            <PrivateRoute path="/movies" exact>
+               <Catalog />
+            </PrivateRoute>
 
             <Redirect from="/auth" to="/auth/login" exact />
             <Route path="/auth">
                <Auth />
             </Route>
 
-            <PrivateRoute path="/movies" exact>
-               <Catalog />
-            </PrivateRoute>
 
             <PrivateRoute path="/movies/:movieId">
                <MovieDetails />
