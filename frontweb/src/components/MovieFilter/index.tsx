4e4ff3832104f3e4a0bc6bce38f4991d02fbd60a
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2022-02-07 11:09:06 -0400

    Retrieving and rendering movies by genre

--- frontweb/src/components/MovieFilter/index.tsx
+++ frontweb/src/components/MovieFilter/index.tsx
@@ -8,17 +8,45 @@ import { requestBackend } from 'utils/requests';
 
 import './styles.css';
 
-type MovieFilterData = {
+/**
+ * Tipo de dados de filtragem.
+ */
+export type MovieFilterData = {
   genre: Genre | null;
 };
 
-const MovieFilter = function () {
-  //
+/**
+ * Tipo de dados dos parâmetros do componente `MovieFilter`.
+ */
+type Props = {
+  onSubmitFilter: (data: MovieFilterData) => void;
+};
+
+const MovieFilter = function (props: Props) {
+  /**
+   * Função `handleSubmitFilter` que será executada para
+   * atualizar os dados de controle referentes a filtragem,
+   * ou seja, `controlComponentsData.filterData.genre`.
+   */
+  const { onSubmitFilter } = props;
+
+  /**
+   * Gêneros de filmes cadastrados no BD que serão usados
+   * na filtragem por gênero.
+   */
   const [genres, setGenres] = useState<Genre[]>();
 
-  const { register, handleSubmit, setValue, getValues, control } =
+  /**
+   * Dados de configuração de formulário de acordo com
+   * o hook `react-hook-form`.
+   */
+  const { handleSubmit, setValue, getValues, control } =
     useForm<MovieFilterData>();
 
+  /**
+   * Hook que recupera do BD os gêneros dos filmes usados
+   * para listá-los.
+   */
   useEffect(() => {
     const config: AxiosRequestConfig = {
       method: 'GET',
@@ -36,25 +64,53 @@ const MovieFilter = function () {
       });
   }, []);
 
+  /**
+   * Função que é executada quando um evento de submissão de formulário
+   * é disparado. Isto é, quando um enter ou botão do formulário é pressionado,
+   * os dados do formulário formam o argumento para a execução da
+   * `handleSubmitFilter`.
+   * @param formData dados que serão submetidos a partir do formulário.
+   */
+  const onSubmit = function (formData: MovieFilterData) {
+    onSubmitFilter(formData);
+  };
+
+  /**
+   * Função que trata a mudança de gênero do filme.
+   * @param value gênero selecionado
+   */
+  const handleChangeGenre = function (value: Genre) {
+    setValue('genre', value);
+
+    const formData: MovieFilterData = {
+      genre: getValues('genre'),
+    };
+
+    onSubmitFilter(formData);
+  };
+
   return (
     <div className="base-card movie-filter-container">
-      <div className="movie-filter-genre">
-        <Controller
-          name="genre"
-          control={control}
-          render={({ field }) => (
-            <Select
-              {...field}
-              classNamePrefix="movie-filter-genre"
-              options={genres}
-              placeholder="Gênero"
-              getOptionLabel={(genre: Genre) => genre.name}
-              getOptionValue={(genre: Genre) => genre.id + ''}
-              isClearable
-            />
-          )}
-        />
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="movie-filter-genre">
+          <Controller
+            name="genre"
+            control={control}
+            render={({ field }) => (
+              <Select
+                {...field}
+                classNamePrefix="movie-filter-genre"
+                options={genres}
+                placeholder="Gênero"
+                getOptionLabel={(genre: Genre) => genre.name}
+                getOptionValue={(genre: Genre) => genre.id + ''}
+                isClearable
+                onChange={(value) => handleChangeGenre(value as Genre)}
+              />
+            )}
+          />
+        </div>
+      </form>
     </div>
   );
 };
--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,27 +1,53 @@
 import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { AxiosRequestConfig } from 'axios';
 
 import { Movie } from 'types/movie';
 import MovieCard from 'components/MovieCard';
 import { SpringPage } from 'types/vendor/spring';
 import { requestBackend } from 'utils/requests';
-import MovieFilter from 'components/MovieFilter';
+import MovieFilter, { MovieFilterData } from 'components/MovieFilter';
 
 import './styles.css';
 
+/**
+ * Tipo de dados que controlam o resultado renderizado pelo componente.
+ * Isto é, a depender da paginação e dos dados de filtragem, os
+ * resultados renderizados serão diferentes.
+ */
+type ControlComponentsData = {
+  filterData: MovieFilterData;
+};
+
 const Catalog = function () {
-  //
+  /**
+   * Página Spring de `movie` carregada a partir do back-end.
+   */
   const [page, setPage] = useState<SpringPage<Movie>>();
 
-  useEffect(() => {
+  /**
+   * Dados de controle do componente.
+   */
+  const [controlComponentsData, setControlComponentsData] =
+    useState<ControlComponentsData>({ filterData: { genre: null } });
+
+  /**
+   * Função que, por fazer uso de um `hook`, está restrita ao
+   * funcionamento do ciclo de vida da aplicação `react`.
+   * A saber, `useCallback` faz com que uma única referẽncia
+   * de função seja armazenada.
+   */
+  const getMovies = useCallback(() => {
+    //
     const config: AxiosRequestConfig = {
-      withCredentials: true,
+      method: 'GET',
       url: '/movies',
       params: {
         page: 0,
-        size: 50,
+        size: 50, // CHANGE later
+        genreId: controlComponentsData.filterData.genre?.id,
       },
+      withCredentials: true,
     };
 
     requestBackend(config)
@@ -29,14 +55,27 @@ const Catalog = function () {
         setPage(response.data as SpringPage<Movie>);
       })
       .catch((error) => {
-        console.log(error);
+        console.log(error); // CHANGE later
       });
     //
-  }, []);
+  }, [controlComponentsData]);
+
+  useEffect(() => {
+    getMovies();
+  }, [getMovies]);
+
+  /**
+   * Função que é executada novos dados de filtragem são submetidos,
+   * para que a listagem de filmes seja atualizada.
+   * @param filterData dados de filtragem
+   */
+  const handleSubmitFilter = function (filterData: MovieFilterData) {
+    setControlComponentsData({ filterData });
+  };
 
   return (
     <div className="catalog-container">
-      <MovieFilter />
+      <MovieFilter onSubmitFilter={handleSubmitFilter} />
 
       <div className="row">
         {page?.content.map((movie) => (
